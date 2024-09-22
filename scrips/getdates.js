9e647fea4f5c111855c26174b20af150093c48af
Your Name <pasapera279@gmail.com>
2024-09-21 23:59:28 -0500

    Homewor week 01, home page

---
+++ scrips/getdates.js
@@ -0,0 +1,7 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+const lastModifiedDate = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModifiedDate}`;
+
