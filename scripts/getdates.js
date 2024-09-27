a01b3cc8f81cf278638da7a8a36661ed8d8bcfa0
Your Name <pasapera279@gmail.com>
2024-09-27 17:19:23 -0500

    to add hamburger menu with js to week02

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,30 +3,16 @@ document.getElementById('currentyear').textContent = currentYear;
 
 const lastModifiedDate = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModifiedDate}`;
-// check understanding ... Loops and iteration
-// const minAge = 18;
-// const days = 6;
-// const limit = 30;
-// let studentReport = [11, 42, 33, 64, 29, 37, 44];
-// const agesReport = [16,33, 29, 15, 19, 40,21, 60, 31, 19, 18, 28, 45]
-// // working wit loop for
 
-// for (let i = 0; i < studentReport.length; i++) {
-//     if(studentReport[i] < limit){
-//         console.log(studentReport[i])
-//     };
-// };
-// //working with while 
-// const i = 0;
-// while ( i < studentReport) {
-//     if(studentReport[1] < limit){
-//         console.log(studentReport[i]);
-//     }
-//     i++
-// };
-// // forEach loop
-// agesReport.forEach(function(item) {
-//    if(item < minAge){
-//     console.log("alumnos con edad menor a 18 años:" + item)
-//    }
-// });
+
+// Store the selected elements that we are going to use. 
+const mainNav = document.querySelector(".nav");
+const hamButton = document.querySelector("#menu");
+
+if (mainNav && hamButton) {
+    hamButton.addEventListener("click", () => {
+        mainNav.classList.toggle("show");  
+        hamButton.classList.toggle("show"); 
+    });
+}
+
