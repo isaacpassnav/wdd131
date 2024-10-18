2b8620d1c2ea82b969ec80565e2840641937a360
Your Name <pasapera279@gmail.com>
2024-10-18 13:00:22 -0500

    Product Review Form to week05

---
+++ scripts/w05.js
@@ -0,0 +1,28 @@
+const products = [
+    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+]
+const selectElement = document.getElementById("product-name");
+products.forEach(product => {
+    const option = document.querySelector("option")
+    option.value = option.id;
+    option.textContent = product.name;
+    selectElement.appendChild(option);
+});
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const reviewCount = localStorage.getItem("reviewCount");
+    if (!reviewCount) {
+        reviewCount = 0;
+    }else{
+        reviewCount = parseInt(reviewCount, 10);
+    }
+    reviewCount ++
+    localStorage.setItem("reviewCount", reviewCount);
+
+    console.log("Cantidad de revisiones completadas:", reviewCount);
+
+});
